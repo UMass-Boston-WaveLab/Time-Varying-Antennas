401b323c6e039a4f1435ac766fd1eec2ad2dbdf1
Lara Kheireddine <32651839+Larakheireddine001@users.noreply.github.com>
2018-05-31 01:43:00 -0400

    Update During_and_end_of_an_on_period.m

--- During_and_end_of_an_on_period.m
+++ During_and_end_of_an_on_period.m
@@ -1,9 +1,9 @@
 f=10e6;
 Omega_o=100e6.*(2.*pi);
-tau=(2.*pi).*f.*10.2;
-tau_prime=((2.*pi).*f)/5;
-Omega_d=sqrt((1/2.*tau) - Omega_o);
-Omega_dprime=sqrt((1/2.*tau_prime)- Omega_o);
+tau=(0.098)/((2.*pi).*f);
+tau_prime=5/((2.*pi).*f);
+Omega_d=sqrt((1/2.*tau).^2 - (Omega_o).^2);
+Omega_dprime=sqrt((1/2.*tau_prime).^2- (Omega_o).^2);
 Omega_c=f.*(2.*pi);
 C=(1/5000e6).*(2.*pi);
 L=5/((2.*pi).*f);
@@ -11,22 +11,27 @@ R_R=1;
 R_L=50;
 R=R_R + R_L;
 T=1/100e6;
-tau_on=T.*(0.1:0.01:0.09);
+tau_on=T.*(0.5);
 tau_off=T - tau_on;
 n=(0:100);
-t=(0.1:0.01:0.09); %Values of t are much larger than values of tau_on since T is a small number. 
-%Which causes the script to ouput all power(ii) = 0. When the values of t get really small,an error at P(ii) = 0 occurs. 
-A_n=(0.01:1);
+t=(0:T/1000:T);
+p=exp(-((tau_on/(2.*tau)) + (tau_off/(2.*tau_prime)))).*cos(Omega_d.*tau_on).*cos(Omega_dprime.*tau_off);
+V_o=1;
 V_s=1;
-V=A_n + V_s.*cos(Omega_c.*n.*T);
+W=zeros(size(n));
+V=zeros(size(n));
 I=zeros(size(t));
 P=zeros(size(t));
-for ii =0.01:length(t)
-    if any(t < tau_on)
-        I(ii)=C.*((V - V_s.*cos(Omega_c.*n.*T)).*((-1/(2.*tau)).*e^(-t/2.*tau).*cos(Omega_d.*t) - Omega_d.*sin(Omega_d.*t).*e^(-t/2.*tau)));
-        P(ii)=I^2.*R;
-    else
-        P(ii)=0; 
+for jj = 1:length(n)
+    for ii =1:length(t)
+            W(jj) = ((p.^(-1).*exp(1i.*(Omega_c.*T)) - p.^(-n(jj)).*exp(1i.*(Omega_c.*n(jj).*T)))/(1-p.^(-1).*exp(1i.*(Omega_c.*T)))) + ((p.^(-1).*exp(-1i.*(Omega_c.*T)) - p.^(-n(jj)).*exp(-1i.*(Omega_c.*n(jj).*T)))/(1-p.^(-1).*exp(-1i.*(Omega_c.*T))));
+            V(jj)= p.^(n(jj)).*V_o + (p.^(n(jj))- p.^(n(jj)+1)).*(V_s/2).*(W(jj));
+        if any(t < tau_on)
+            I(ii,jj)=C.*((V(jj) - V_s.*cos(Omega_c.*n(jj).*T)).*((-1/(2.*tau)).*e^(-t(ii)/2.*tau).*cos(Omega_d.*t(ii)) - Omega_d.*sin(Omega_d.*t).*e^(-t(ii)/2.*tau)));
+            P(ii,jj)=I(ii,jj)^2.*R_L;
+        else
+        P(ii,jj)=0; 
+        end
     end
 end
 plot(t,P);
